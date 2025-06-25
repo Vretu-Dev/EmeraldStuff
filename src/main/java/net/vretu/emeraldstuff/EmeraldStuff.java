0c4ee3aab267e6e0596c30fabf1f13a7a30d79d9
Vretu-Dev <jakub.spieczny1@gmail.com>
2025-06-25 14:35:09 +0200

    Initial commit

---
+++ src/main/java/net/vretu/emeraldstuff/EmeraldStuff.java
@@ -0,0 +1,15 @@
+package net.vretu.emeraldstuff;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EmeraldStuff implements ModInitializer {
+	public static final String MOD_ID = "emeraldstuff";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/vretu/emeraldstuff/EmeraldStuffClient.java
@@ -0,0 +1,10 @@
+package net.vretu.emeraldstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EmeraldStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
